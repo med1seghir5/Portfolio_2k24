5eaff2f5960d238b3ff0e2c5e5b6bf69ffb496d6
Abdelmalek Mohamed_Seghir <135925192+med1seghir5@users.noreply.github.com>
2024-02-02 12:31:07 +0100

    Add files via upload

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -3,8 +3,8 @@ import pic from './images/pic.svg';
 
 const Navbar = () => {
   return (
-    <div className='flex flex-row space-x-28 ml-12 space-y-7 sm:flex-row items-center sm:justify-between space-y-4 sm:space-y-0 sm:space-x-8 ml-4 sm:ml-32' id='page'>
-      <div className='ml-20 pt-10 pb-8 sm:mb-0'>
+    <div className='flex flex-row space-x-28 space-y-7 justify-center sm:flex-row items-center sm:justify-between space-y-4 sm:space-y-0 sm:space-x-8 ml-4 sm:ml-32' id='page'>
+      <div className='pt-10 pb-8 sm:mb-0'>
         <img src={pic} className='w-60' alt='Profile Pic' />
       </div>
     </div>
